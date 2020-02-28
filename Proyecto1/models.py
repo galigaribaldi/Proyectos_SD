127e3da5a3ecdeab440a67f6ed3a6232a9d90268
galigaribaldi <galigaribaldi@live.com>
2020-02-27 20:28:51 -0600

    Cambio 1 de consultas

--- Proyecto1/models.py
+++ Proyecto1/models.py
@@ -1,5 +1,6 @@
 import pymysql
-
+from datetime import date
+today = date.today()
 def consulta_usuario(usuario_id):
     mariadb_conexion = pymysql.connect('localhost','gali','12345','base1')
     cursor = mariadb_conexion.cursor()
@@ -32,6 +33,15 @@ def consulta_cuenta_all():
     mariadb_conexion = pymysql.connect('localhost','gali','12345','base1')
     cursor = mariadb_conexion.cursor()
     cursor.execute("SELECT * FROM CUENTA_BANCARIA")
+    datos = cursor.fetchall()
+    mariadb_conexion.commit()
+    mariadb_conexion.close()
+    return datos
+#
+def actualiza_fecha(cuenta_id):
+    mariadb_conexion = pymysql.connect('localhost','gali','12345','base1')
+    cursor = mariadb_conexion.cursor()
+    cursor.execute("UPDATE CUENTA_BANCARIA SET FECHA_CONSULTA= %s WHERE CUENTA_BANCARIA_ID= %s",(today,cuenta_id, ))
     datos = cursor.fetchall()
     mariadb_conexion.commit()
     mariadb_conexion.close()
